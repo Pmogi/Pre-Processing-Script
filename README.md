46f1ab5cdbc16bd619069f8fb0402488ff14204e
Patrick Mogianesi <pmogiane@ucsc.edu>
2017-10-09 17:42:27 -0700

    Made some small corrections for orginization

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # Pre-Processing-Script
-A script for my REU at IIT I wrote that allows the user to find a file using tkinter which is then passed to VirtualDub via cmd for editing.
+Patrick Mogianesi
+pmogiane@ucsc.edu
+
+########################################
+
+Purpose:
+A script to process videos to be readable by dicg motion-correction software.
+
+video -> program -> edited video ready for motion correction
+
+Required Software:
+- Python 3.X
+- VirtualDub Video editor
+- .jobs files that contains the script for vdub processs
+- Windows computer*
+
+Optional:
+- DubMan, a handy tool that creates scripts for VirtualDub for batch processes. Beats having to tussle with
+the awful Vdub scripting language.
+
+**quickEdit.py starts the program**
+
+*warning* If files get moved around/filenames change, you may need to change some Paths in the python/vdub script.
+Most of this script is honestly me moving things around through accessing the windows commandline through python.
+So you'll have to change the path's if you try using this on a different pc or move files around.
+
+-------
+-Using the script-
+
+- When initilizing the script it will ask the user for an .avi file to process in a graphical interface.
+
+- The program will ask the user if they are using rat data or non-rat data.
+  + The input is in the open commandline window.
+
+  + There are two scripts used based on the user input.
+    / First one is meant for rat data that decimates frames and makes sure it's 256 x 256.
+    
+    / Second one is meant for non-rat data and only resizes the video's resolution.
+
+- This .avi file is sent to the VirtualDub software via command-line.
+
+- The edited movie will be saved in the quickEdit folder with _edited at the end of the filename
+
+- This edited movie should be optimized enough for the motion-correction software.
+
+
+-notes-
+For the rat data videos, there are a lot of unneccesary frames; so I opted to decimate 99 frames for every frame.
+Through some experimentation, I did find that I can decimate as little as 50 frames and still have it load into
+the motion-correction software. I'm still not sure the specific requirements for file size for the files to be
+function properly in the program, but the max file-size that seemed to work through experimentation is ~60 MB.
+The Dubman script can be adjusted accordingly to the rate of decimation.
+
+*details are in python script
+
+All software used is under the GNU license
--- quickEdit/main.py
+++ quickEdit/main.py
@@ -1,5 +1,5 @@
 # Patrick Mogianesi REU Summer Student 2017
-# 7/12/17
+# July 12 - 20, 2017
 # Python 3.X required & virtualdub software
 # Unfortunatly becuase of the virtualdub software, this will only work on windows
 
@@ -11,45 +11,73 @@
 from tkinter import filedialog # gets file information w/ gui like matlab
 from tkinter import *
 import os # for command line actions
-import time # just in case I need pauses
 
 # initalizing Tk object
 guiObj = Tk()
 # get path to file using graphic interface
 # source for more information
 # https://pythonspot.com/en/tk-file-dialogs/
-guiObj.file = filedialog.askopenfilename(initialdir = "C:\\Users\ktichauer\Desktop" , title = "Select AVI File To Process", filetypes = [("AVI files", "*.avi")]) #similar gui to file picking with matlab
-
-programLocation = "\"C:\\Users\ktichauer\Desktop\quickEdit\movie2edit.avi\""
-selectedVideo = guiObj.file
-
-print(programLocation)
+guiObj.file = filedialog.askopenfilename(initialdir = "C:\\Users\ktichauer\Desktop" , title = "Select AVI File To Process", filetypes = [("AVI files", "*.avi")]) #similar gui to file picking with matlab, if different pc, switch to ur desktop
+programLocation = "\"C:\\Users\ktichauer\Desktop\quickEdit\movie2edit.avi\"" # if different pc change to quickEdit location and save as movie2edit
+selectedVideo = guiObj.file # picking a more friendly variable for the file pathname
 print(selectedVideo)
+print(programLocation)
+
+
+# change forward slashes to black slashes so cmd can use the path
 selectedVideo = list(selectedVideo)
+for i in range(0,len(selectedVideo)): # for-loop through the lenth of the filepath
+    if selectedVideo[i] == '/':
+        selectedVideo[i] = '\\'
+selectedVideo = ''.join(selectedVideo) # after changing the forward slashes to back slashes, make the list into a string again
+print(selectedVideo)
+
 
 # this function gets the name of the video
 def getVideoName(videoList):
-    i = (len(videoList) - 1)
-    fileName = []
+    i = (len(videoList)-5)
+    fileName = [] # this list will hold the filename
     while videoList[i] != "\\": #loop until it hits the directory slash
         fileName.append(videoList[i])
         i = i - 1
-    ''.join(fileName)
-    return fileName
+    fileName = fileName[::-1] # reverse the list, because the method above actually messes up the title lol
+    for i in range(0, len(fileName)):
+        if fileName[i] == " ":
+            fileName[i] = "_" # this makes sure there are no spaces in the file's name, spaces gave me a lot of grief when renaming the edited file
+    
+    fileName = ''.join(fileName) # this list filename to a string
+    fileName = fileName + "_edited.avi" # change original filename to indicate this is the edited file
+    return fileName # returns the string of the filename
 # use the function defined above
 fileName = getVideoName(selectedVideo)
 print(fileName)
-# change forward slashes to black slashes so cmd can use the path
-for i in range(0,len(selectedVideo)):
-    if selectedVideo[i] == '/':
-        selectedVideo[i] = '\\'
-selectedVideo = ''.join(selectedVideo)
-print(selectedVideo)
+
+
 selectedVideo = "\"" + selectedVideo + "\"" # make sure the file has "" around the path, if there are spaces and no apostrophes, cmd cannot read that as a path
+
 
 # test to see filepath
 print ("Filepath: " + selectedVideo)
-print ("copy " + " " +selectedVideo + " " + programLocation)
+print ("copy " + " " + selectedVideo + " " + programLocation)
+print("Copying files please wait...")
 os.system("copy " + " " + selectedVideo + " " + programLocation) # this will move the selected movie to quickEdit renaming it to movie2edit.avi so I have a common name to use in the virtualdub script
-os.system(".\\vdub.exe /i fixerScript.jobs movie2edit.avi editedMovie.avi") # use virtualDub to cut down and resize the video
-os.system('del /f movie2edit.avi') # delete the movie2edit, it's redundant now that the video has been fixed up in virtualDub
+
+def pass2VirtualDub():
+    response = input("Are you fixing Rat Data? y or n > ")
+    if response == "y":
+        os.system(".\\vdub.exe /i fixerScript.jobs movie2edit.avi editedMovie.avi") # pass video to virtualdub using rat data 'fixerScript' reanme video as editedMovie
+
+    elif response == "n": # if it isn't rat data, it probably just needs to be resized to 256x256
+        os.system(".\\vdub.exe /i fixerScriptNotRat.jobs movie2edit.avi editedMovie.avi") # pass video to virtualdub using non-rat data 'fixerScript'
+
+    else:
+        print("That is not a valid input.")
+        pass2VirtualDub() # send user back to start of function and make a valid
+
+# use function defined above
+pass2VirtualDub()
+
+os.system('del /f movie2edit.avi') # delete the movie2edit .avi file, it's redundant now that the video has been fixed up in virtualDub
+
+# rename the file as the original name with _edited at the end
+os.system('rename' + " " + "editedMovie.avi" + " " + fileName)
